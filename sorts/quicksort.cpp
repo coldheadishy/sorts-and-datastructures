782fac73bc25a3beaa776196181fc4c894436084
Rahul <rahul95mehta@gmail.com>
2017-06-18 19:07:58 -0400

    CHANGED: full implementation
    Changed code to be a full implementaion

--- sorts/quicksort.cpp
+++ sorts/quicksort.cpp
@@ -1,27 +1,36 @@
-//quicksort - O(n^2) 
-void quickSort(int arr[], int left, int right) {
-      int i = left, j = right;
-      int tmp;
-      int pivot = arr[(left + right) / 2];
- 
-      /* divide */
-      while (i <= j) {
-            while (arr[i] < pivot)
-                  i++;
-            while (arr[j] > pivot)
-                  j--;
-            if (i <= j) {
-                  tmp = arr[i];
-                  arr[i] = arr[j];
-                  arr[j] = tmp;
-                  j--;
-                  i++;    
-            }
-      };
- 
-      /* conquer recursion */
-      if (left < j)
-            quickSort(arr, left, j);
-      if (i < right)
-            quickSort(arr, i, right);
+#include <iostream>
+#include <algorithm>
+
+void print(int *a, int n)
+{
+    int i = 0;
+    while(i < n){
+        std::cout << a[i] << ",";
+        i++;
+    }
+    std::cout << "\n";
+}
+
+int partition(int *arr, const int left, const int right) {
+    const int mid = left + (right - left) / 2;
+    const int pivot = arr[mid];
+    // move the mid point value to the front.
+    std::swap(arr[mid],arr[left]);
+    int i = left + 1;
+    int j = right;
+    while (i <= j) {
+        while(i <= j && arr[i] <= pivot) {
+            i++;
+        }
+
+        while(i <= j && arr[j] > pivot) {
+            j--;
+        }
+
+        if (i < j) {
+            std::swap(arr[i], arr[j]);
+        }
+    }
+    std::swap(arr[i - 1],arr[left]);
+    return i - 1;
 }
